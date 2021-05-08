0d5bf4e5e2202d75f0462ae099636d39cfaeda62
Your Name <k.zeidlere@gmail.com>
2021-05-08 21:09:20 +0300

    User project

---
+++ add_user/urls.py
@@ -0,0 +1,27 @@
+"""add_user URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+import user.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', user.views.UserListView.as_view()),
+    path('add_user', user.views.AddUserView.as_view()),
+    path('get_user/<int:pk>', user.views.UserDetailView.as_view(), name='user-detail'),
+    path('edit_user/<int:pk>', user.views.EditUserView.as_view(), name='user-id'),
+    path('delete_user/<int:pk>', user.views.UserDeleteView.as_view()),
+]
